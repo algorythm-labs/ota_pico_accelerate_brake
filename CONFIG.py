30c2e0e982d53e9dc691042dff74ebcaffa40708
Daniel Wilén <daniel@minneslucka.com>
2024-05-05 10:42:00 +0200

    Adding source code

---
+++ CONFIG.py
@@ -0,0 +1,6 @@
+MODULE = "accelerate_brake"
+PORT = 80
+SSID = "ota_lab"
+PASSWORD = "ota_lab_password"
+URL = "https://raw.githubusercontent.com/algorythm-labs/ota_pico_accelerate_brake/main/"
+FILE = "accelerate_brake.py"
